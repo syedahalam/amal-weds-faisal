cec471f430a5e53c833d3188c3806195c7ff3d6e
syedahalam <syedahalam@yahoo.com>
2022-01-31 01:04:57 -0500

    media queries

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Make MOBILE nav work
+
+const btnNav = document.querySelector('.btn-mobile-nav');
+const headerNav = document.querySelector('.header');
+
+btnNav.addEventListener('click', function () {
+	headerNav.classList.toggle('.nav-open');
+});
+
+////////////////////smooth scrolling animation/////////////
+const allLinks = document.querySelectorAll('a:link');
+console.log(allLinks);
+allLinks.forEach(function (link) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+		const href = link.getAttribute('href');
+		console.log(href);
+
+		/////scroll back to top/////////////////
+		if (href === '#')
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth',
+			});
+		if (href !== '#' && href.startsWith('#')) {
+			const sectionEl = document.querySelector(href);
+			// console.log(sectionEl);
+			sectionEl.scrollIntoView({ behavior: 'smooth' });
+		}
+		///////////////////close mobile nav////////////
+		if (link.classList.contains('main-nav-link'))
+			headerNav.classList.toggle('nav-open');
+	});
+});
+
+// timer////////////
 const countDown = () => {
 	const countDate = new Date('June 05, 2022 16:00:00').getTime();
 	// console.log(countDate);
