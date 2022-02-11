07abc28f902546beaa8a2b6045d6fd4b07157abe
syedahalam <syedahalam@yahoo.com>
2022-02-11 01:00:58 -0500

    tiny gallery

--- script.js
+++ script.js
@@ -74,3 +74,31 @@ labels.forEach((label) => {
 		)
 		.join('');
 });
+// ////////////// gallery/////////////
+
+const slider = tns({
+	container: '.my-slider',
+	slideBy: '1',
+	speed: 400,
+	nav: false,
+	autoplay: true,
+	controls: false,
+	autoplayButtonOutput: false,
+	responsive: {
+		1600: {
+			items: 5,
+			gutter: 20,
+		},
+		1024: {
+			items: 4,
+			gutter: 20,
+		},
+		768: {
+			items: 3,
+			gutter: 20,
+		},
+		468: {
+			items: 1,
+		},
+	},
+});
